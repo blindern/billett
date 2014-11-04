72a65a4ebc440e65c02264c484d8edeead04eea6
Henrik Steen <henrist@henrist.net>
2014-11-04 06:37:17 +0100

    Directive to help form validation.

--- public/assets/src/javascript/helper.page.js
+++ public/assets/src/javascript/helper.page.js
@@ -1,16 +1,17 @@
 'use strict';
 
-angular.module('billett.helper.page', ['ngAnimate']).
-factory('Page', function($rootScope) {
+var mod = angular.module('billett.helper.page', ['ngAnimate']);
+
+mod.factory('Page', function($rootScope) {
 	$rootScope.title = 'default';
 	return {
 		title: function() { return $rootScope.title; },
 		setTitle: function(newTitle) { $rootScope.title = newTitle; }
 	};
-}).
+});
 
 // the menu
-controller('HeaderController', function($scope, $location) {
+mod.controller('HeaderController', function($scope, $location) {
 	$scope.isActive = function(path, prefixpath) {
 		if ($location.path() == path)
 			return true;
@@ -24,5 +25,27 @@ controller('HeaderController', function($scope, $location) {
 		}
 
 		return false;
+	};
+});
+
+// helper directive to mark a form input with has-error class
+// usage: <div form-input-check="form-name,input-name">
+mod.directive('formInputCheck', function() {
+	return {
+		restrict: 'A',
+		link: function(_scope, _element, _attrs) {
+			var t = _attrs['formInputCheck'].split(","),
+				form = _scope[t[0]], input = _scope[t[0]][t[1]];
+
+			function recheck() {
+				if (input.$invalid && (input.$dirty || form.$submitted))
+					_element.addClass('has-error');
+				else
+					_element.removeClass('has-error');
+			}
+
+			_scope.$watch(form.$name+'.'+input.$name+'.$invalid', recheck);
+			_scope.$watch(form.$name+'.$submitted', recheck);
+		}
 	};
 });
