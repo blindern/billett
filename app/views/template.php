c35bb66e7b0e0b0725e8e6a6e758cd1f1801f121
Henrik Steen <henrist@henrist.net>
2014-12-11 18:52:40 +0100

    Fix meta fragment attribute-name for content

--- app/views/template.php
+++ app/views/template.php
@@ -14,7 +14,7 @@ $is_dev = (bool)\Config::get('app.dev');
     <meta name="description" content="">
     <meta name="author" content="">
     <base href="<?=app('request')->getBaseUrl();?>/">
-    <meta name="fragment" value="!">
+    <meta name="fragment" content="!">
 
     <link rel="icon" type="image/x-icon" href="favicon.ico">
 
