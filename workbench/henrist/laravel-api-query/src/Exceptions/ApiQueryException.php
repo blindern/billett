e5a4660b95297c2fd128501c84185cf3a6840d1d
Henrik Steen <henrist@henrist.net>
2014-12-17 14:23:29 +0100

    Remove local workbench package, load from new git repo instaed

--- workbench/henrist/laravel-api-query/src/Exceptions/ApiQueryException.php
+++
@@ -1,5 +0,0 @@
-<?php namespace Henrist\LaravelApiQuery\Exceptions;
-
-class ApiQueryException extends \Exception {
-
-}
--- workbench/henrist/laravel-api-query/src/Processors/ProcessorInterface.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Henrist\LaravelApiQuery\Processors;
-
-use Henrist\LaravelApiQuery\Handler;
-use Illuminate\Http\Request;
-
-interface ProcessorInterface {
-    public function process(Handler $handler, Request $request);
-}
