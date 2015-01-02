8619a3fce1a1c4a64b5dd586848245e2091f3b68
Henrik Steen <henrist@henrist.net>
2015-01-02 23:54:45 +0100

    Capture transaction automatically

--- app/config/dibs.php
+++ app/config/dibs.php
@@ -3,7 +3,7 @@
 return array(
     'hmac_key' => 'overridden-in-local-configuration',
     'merchant_id' => 90053779,
-    'capture' => false,
+    'capture' => true,
     'checkout_url' => 'https://sat1.dibspayment.com/dibspaymentwindow/entrypoint',
     'test' => true,
     'currency_id' => 578, // NOK
