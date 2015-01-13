76e43a6187e5501e66a0a1456ebc17a702370eb1
Henrik Steen <henrist@henrist.net>
2015-01-13 03:32:40 +0100

    Fix foreign keys in tickets table

---
+++ app/database/migrations/2015_01_13_032831_tickets_fix_paymentgroup.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TicketsFixPaymentgroup extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tickets', function(Blueprint $table) {
+            $table->dropForeign('tickets_valid_paymentgroup_id_foreign');
+            $table->dropForeign('tickets_revoked_paymentgroup_id_foreign');
+            $table->foreign('valid_paymentgroup_id')->references('id')->on('paymentgroups');
+            $table->foreign('revoked_paymentgroup_id')->references('id')->on('paymentgroups');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tickets', function(Blueprint $table)
+        {
+            $table->dropForeign('tickets_valid_paymentgroup_id_foreign');
+            $table->dropForeign('tickets_revoked_paymentgroup_id_foreign');
+            $table->foreign('valid_paymentgroup_id')->references('id')->on('eventgroups');
+            $table->foreign('revoked_paymentgroup_id')->references('id')->on('eventgroups');
+        });
+    }
+
+}
