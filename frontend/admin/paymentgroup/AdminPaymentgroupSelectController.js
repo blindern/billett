d10ce8dedbcf2f39acb3bbc676affe48856b7c62
Henrik Steen <henrist@henrist.net>
2015-01-16 05:03:08 +0100

    Refactor paymentgroup select modal to use paymentgroup directive

--- frontend/admin/paymentgroup/AdminPaymentgroupSelectController.js
+++ frontend/admin/paymentgroup/AdminPaymentgroupSelectController.js
@@ -1,20 +1,11 @@
-angular.module('billett.admin').controller('AdminPaymentgroupSelectController', function ($http, $modalInstance, AdminPaymentgroup, amount, order, actionText) {
+angular.module('billett.admin').controller('AdminPaymentgroupSelectController', function ($http, $modalInstance, amount, order, actionText) {
     var ctrl = this;
     ctrl.order = order;
     ctrl.actionText = actionText;
     ctrl.amount = amount;
 
-    AdminPaymentgroup.getValid(ctrl.order.eventgroup.id).$promise.then(function (ret) {
-        ctrl.paymentgroups = ret;
-        ctrl.active_paymentgroup = AdminPaymentgroup.getPreferredGroup(ret);
-    });
-
-    ctrl.changePaymentgroup = function () {
-        AdminPaymentgroup.setPreferredGroup(ctrl.active_paymentgroup);
-    };
-
     ctrl.complete = function () {
-        $modalInstance.close(ctrl.active_paymentgroup);
+        $modalInstance.close(ctrl.paymentgroup);
     };
 
     ctrl.cancel = function () {
