5b87d87ac9da837ec80c1708c11ce9818efac6ba
Henrik Steen <henrist@henrist.net>
2015-01-17 05:39:06 +0100

    Fix link to create new paymentgroup

--- frontend/admin/paymentgroup/AdminPaymentgroupListController.js
+++ frontend/admin/paymentgroup/AdminPaymentgroupListController.js
@@ -1,14 +1,24 @@
 angular.module('billett.admin').controller('AdminPaymentgroupListController', function ($stateParams, AdminEventgroup, AdminPaymentgroup, Page) {
     var ctrl = this;
 
-    var loader = Page.setLoading();
-    AdminEventgroup.get({id: $stateParams['eventgroup_id']}, function (ret) {
-        ctrl.eventgroup = ret;
+    var loadPaymentgroups = function () {
+        var loader = Page.setLoading();
+        AdminEventgroup.get({id: $stateParams['eventgroup_id']}, function (ret) {
+            ctrl.eventgroup = ret;
 
-        AdminPaymentgroup.query({filter: 'eventgroup_id='+ret.id}, function (groups) {
-            loader();
+            AdminPaymentgroup.query({filter: 'eventgroup_id='+ret.id}, function (groups) {
+                loader();
 
-            ctrl.paymentgroups = groups;
+                ctrl.paymentgroups = groups;
+            });
         });
-    });
+    };
+
+    ctrl.new = function () {
+        AdminPaymentgroup.newModal(ctrl.eventgroup.id).result.then(function () {
+            loadPaymentgroups();
+        });
+    };
+
+    loadPaymentgroups();
 });
