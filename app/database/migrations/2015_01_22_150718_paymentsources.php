86e43aae93e0e27ab45fa8b0ad478315092e60fe
Henrik Steen <henrist@henrist.net>
2015-01-22 15:14:02 +0100

    Migration for paymentsources

---
+++ app/database/migrations/2015_01_22_150718_paymentsources.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Paymentsources extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paymentsources', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->integer('paymentgroup_id')->unsigned();
+            $table->foreign('paymentgroup_id')->references('id')->on('paymentgroups');
+
+            $table->boolean('is_deleted')->default(false);
+
+            $table->integer('time_created')->unsigned(); // unix timestamp
+            $table->integer('time_deleted')->unsigned()->nullable(); // unix timestamp
+
+            $table->string('user_created')->nullable()->nullable();
+            $table->string('user_deleted')->nullable()->nullable();
+
+            $table->enum('type', array('cash', 'other'))->default('other');
+
+            $table->string('title');
+            $table->text('comment')->nullable();
+            $table->decimal('amount', 7, 2);
+            $table->text('data')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('paymentsources');
+    }
+
+}
