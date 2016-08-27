b216d7d2e23c3eb22ae1f01f187d3f9f438267b5
Henrik Steen <henrist@henrist.net>
2016-08-27 05:08:54 +0200

    Fix urlpath protocol

--- simplesamlphp/config.override.php
+++ simplesamlphp/config.override.php
@@ -1,7 +1,11 @@
 <?php
 
+// we run behind a reverse proxy (nginx) that does the ssl job..
+// we don't need to bother checking $_SERVER['HTTPS']
+$proto = isset($_SERVER['HTTP_X_FORWARDED_PROTO']) ? $_SERVER['HTTP_X_FORWARDED_PROTO'] : 'http';
+
 $config = array_merge($config, array(
-    'baseurlpath' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . '/billett/saml/',
+    'baseurlpath' => $proto . '://' . $_SERVER['HTTP_HOST'] . '/billett/saml/',
     'auth.adminpassword' => getenv('SIMPLESAMLPHP_ADMINPASS'),
     'secretsalt' => getenv('SIMPLESAMLPHP_SECRETSALT'),
     'technicalcontact_name' => 'IT-gruppa',
