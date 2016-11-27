2ffdfd9cd46816ef5b541f63b15a593aad335535
Henrik Steen <henrist@henrist.net>
2016-11-27 23:22:49 +0100

    Override Dibs settings

--- src/app/config/dibs.php
+++ src/app/config/dibs.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-    'hmac_key' => 'overridden-in-local-configuration',
+    'hmac_key' => isset($_ENV['DIBS_HMAC_KEY']) ? $_ENV['DIBS_HMAC_KEY'] : 'overridden-in-local-configuration',
     'merchant_id' => 90053779,
     'capture' => true,
     'checkout_url' => 'https://sat1.dibspayment.com/dibspaymentwindow/entrypoint',
