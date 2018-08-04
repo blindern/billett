393b5ed8efff84d89afd20d5809090b64606a484
Henrik Steen <henrist@henrist.net>
2018-08-04 17:54:58 +0200

    Fix docker login command in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -41,7 +41,7 @@ ssh -p 2222 -o StrictHostKeyChecking=no core@server2016.blindernuka.no /bin/bash
   git fetch
   git checkout $TRAVIS_COMMIT
 
-  docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
   time docker build -t blindernuka/billett-backend:$tag -t blindernuka/billett-backend:$commit .
   time docker push blindernuka/billett-backend:$tag
   time docker push blindernuka/billett-backend:$commit
