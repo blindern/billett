bd7835491785e692d2da232e280f37c4672529ac
Henrik Steen <henrist@henrist.net>
2022-07-07 13:36:51 +0200

    Re-add plugin to add ng annotations
    This fixes the prod build.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
+import babel from "vite-plugin-babel"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  plugins: [
+    babel({
+      babelConfig: {
+        babelrc: false,
+        configFile: false,
+        plugins: ["angularjs-annotate"],
+      },
+    }),
+  ],
   define: {
     DEBUG: !!process.env.DEBUG ?? false,
     BACKEND_URL: JSON.stringify(process.env.BACKEND_URL ?? "/"),
