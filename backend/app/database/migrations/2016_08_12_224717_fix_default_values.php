ed50d8685268d216833e28be7f9193808f744293
Henrik Steen <henrist@henrist.net>
2024-07-08 11:24:49 +0200

    Convert remaining tabs to spaces

--- backend/app/database/migrations/2016_08_12_224717_fix_default_values.php
+++ backend/app/database/migrations/2016_08_12_224717_fix_default_values.php
@@ -14,17 +14,17 @@ class FixDefaultValues extends Migration
         DB::statement('ALTER TABLE events MODIFY image blob DEFAULT NULL');
 
         DB::statement('ALTER TABLE orders MODIFY order_text_id varchar(30) DEFAULT NULL,
-			                              MODIFY is_locked tinyint(1) NOT NULL DEFAULT 0,
-			                              MODIFY is_valid tinyint(1) NOT NULL DEFAULT 0,
-			                              MODIFY balance decimal(7,2) NOT NULL DEFAULT 0');
+                                          MODIFY is_locked tinyint(1) NOT NULL DEFAULT 0,
+                                          MODIFY is_valid tinyint(1) NOT NULL DEFAULT 0,
+                                          MODIFY balance decimal(7,2) NOT NULL DEFAULT 0');
 
         DB::statement('ALTER TABLE payments MODIFY paymentgroup_id int(10) unsigned DEFAULT NULL');
 
         DB::statement('ALTER TABLE ticketgroups MODIFY use_web tinyint(1) NOT NULL DEFAULT 0');
 
         DB::statement('ALTER TABLE tickets MODIFY is_valid tinyint(1) NOT NULL DEFAULT 0,
-			                               MODIFY is_revoked tinyint(1) NOT NULL DEFAULT 0,
-			                               MODIFY `key` varchar(30) DEFAULT NULL');
+                                           MODIFY is_revoked tinyint(1) NOT NULL DEFAULT 0,
+                                           MODIFY `key` varchar(30) DEFAULT NULL');
 
         /* cannot use with Larvel 4.2
         Schema::table('orders', function ($table)
@@ -58,17 +58,17 @@ class FixDefaultValues extends Migration
         DB::statement('ALTER TABLE events MODIFY image blob');
 
         DB::statement('ALTER TABLE orders MODIFY order_text_id varchar(30),
-			                              MODIFY is_locked tinyint(1) NOT NULL,
-			                              MODIFY is_valid tinyint(1) NOT NULL,
-			                              MODIFY balance decimal(7,2) NOT NULL');
+                                          MODIFY is_locked tinyint(1) NOT NULL,
+                                          MODIFY is_valid tinyint(1) NOT NULL,
+                                          MODIFY balance decimal(7,2) NOT NULL');
 
         DB::statement('ALTER TABLE payments MODIFY paymentgroup_id int(10) unsigned NOT NULL');
 
         DB::statement('ALTER TABLE ticketgroups MODIFY use_web tinyint(1) NOT NULL');
 
         DB::statement('ALTER TABLE tickets MODIFY is_valid tinyint(1) NOT NULL,
-			                               MODIFY is_revoked tinyint(1) NOT NULL,
-			                               MODIFY `key` varchar(30) NOT NULL');
+                                           MODIFY is_revoked tinyint(1) NOT NULL,
+                                           MODIFY `key` varchar(30) NOT NULL');
 
         /* cannot use with Laravel 4.2
         Schema::table('events', function ($table)
