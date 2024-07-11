026e49375b2de37aabe937cda666b3abebf63bed
Henrik Steen <henrist@henrist.net>
2024-07-11 15:47:36 +0200

    Fix logout

--- frontend/src/auth/index.js
+++ frontend/src/auth/index.js
@@ -18,8 +18,18 @@ module.config(function ($stateProvider) {
     .state("logout", {
       url: "/logout",
       template: "Logger ut",
-      controller: function () {
-        window.location.href = api("logout")
+      controller: function (AuthService) {
+        AuthService.getCsrfToken().then((csrfToken) => {
+          const form = document.createElement("form")
+          form.method = "post"
+          form.action = api(
+            `saml2/logout?_token=${encodeURIComponent(
+              csrfToken,
+            )}&returnTo=${encodeURIComponent(window.location.origin + "/")}`,
+          )
+          document.body.append(form)
+          form.submit()
+        })
       },
     })
 })
