eacdf64ff097556ab886640420490fa0c2ff6b1c
Henrik Steen <henrist@henrist.net>
2024-07-12 17:19:59 +0200

    Migrate salgsbetingelser

--- frontend/src/app.routes.ts
+++ frontend/src/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from "@angular/router"
 import { GuestIndexComponent } from "./guest/index/index.component"
 import { GuestEventgroupComponent } from "./guest/eventgroup/eventgroup.component"
 import { NotFoundComponent } from "./guest/infopages/not-found.component"
+import { SalgsbetingelserComponent } from "./guest/infopages/salgsbetingelser.component"
 
 export const routes: Routes = [
   {
     path: "",
     component: GuestIndexComponent,
+  },
+  {
+    path: "salgsbetingelser",
+    component: SalgsbetingelserComponent,
+    title: "Salgsbetingelser",
   },
   {
     path: "eventgroup/:id",
---
+++ frontend/src/guest/infopages/salgsbetingelser.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "salgsbetingelser",
+  standalone: true,
+  imports: [],
+  templateUrl: "./salgsbetingelser.component.html",
+})
+export class SalgsbetingelserComponent {
+  constructor() {}
+}
