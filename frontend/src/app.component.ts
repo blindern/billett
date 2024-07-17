e69619e94b22f169ed73d61b8d84f7d076be6fdf
Henrik Steen <henrist@henrist.net>
2024-07-17 04:49:13 +0200

    Migrate auto-close of dialogs on route change

--- frontend/src/app.component.ts
+++ frontend/src/app.component.ts
@@ -1,6 +1,13 @@
+import { Dialog } from "@angular/cdk/dialog"
 import { AsyncPipe, Location } from "@angular/common"
-import { afterRender, Component, OnInit } from "@angular/core"
-import { RouterLink, RouterOutlet } from "@angular/router"
+import { afterRender, Component, inject, OnInit } from "@angular/core"
+import {
+  Event,
+  EventType,
+  Router,
+  RouterLink,
+  RouterOutlet,
+} from "@angular/router"
 import { AuthService } from "./auth/auth.service"
 import { ActiveFor } from "./common/active-for"
 
@@ -11,15 +18,17 @@ import { ActiveFor } from "./common/active-for"
   templateUrl: "./app.component.html",
 })
 export class AppComponent implements OnInit {
+  private location = inject(Location)
+  public auth = inject(AuthService)
+  private router = inject(Router)
+  private dialog = inject(Dialog)
+
   loggedInButNoAccess = false
   isAdminPage = () => {
     return this.location.path().substring(0, 3) == "/a/"
   }
 
-  constructor(
-    private location: Location,
-    public auth: AuthService,
-  ) {
+  constructor() {
     afterRender(() => {
       if (window.top != window.self) {
         document.body.classList.add("isInIframe")
@@ -42,6 +51,15 @@ export class AppComponent implements OnInit {
             this.loggedInButNoAccess = true
           }
         })
+      }
+    })
+
+    // make sure modals close on state change
+    this.router.events.subscribe((s: Event) => {
+      if (s.type === EventType.NavigationStart) {
+        for (const dialog of this.dialog.openDialogs) {
+          dialog.close()
+        }
       }
     })
   }
