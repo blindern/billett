06052b2f212217b5b31fc38188cf90e5ed57b9db
Henrik Steen <henrist@henrist.net>
2024-07-19 20:07:17 +0200

    Fix init hook for NavigationService

--- frontend/src/common/navigation.service.ts
+++ frontend/src/common/navigation.service.ts
@@ -1,18 +1,18 @@
 import { Location } from "@angular/common"
-import { inject, Injectable, OnInit } from "@angular/core"
+import { inject, Injectable } from "@angular/core"
 import { EventType, Router } from "@angular/router"
 
 @Injectable({
   providedIn: "root",
 })
-export class NavigationService implements OnInit {
+export class NavigationService {
   private router = inject(Router)
   private location = inject(Location)
 
   private history: string[] = []
 
-  ngOnInit(): void {
-    this.router.events.subscribe((event) => {
+  constructor(router: Router) {
+    router.events.subscribe((event) => {
       if (event.type === EventType.NavigationEnd) {
         this.history.push(event.urlAfterRedirects)
       }
